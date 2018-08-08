494076b329a9a6f54bb7772d294bea8a878915b3
Thibault <thibault.dufeu@gmail.com>
2018-08-08 16:54:26 +0200

    commit de la class login

--- src/main/java/model/Login.java
+++ src/main/java/model/Login.java
@@ -1,12 +1,34 @@
 package model;
 
-public class Login {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-	private Long id_login;
+@Entity
+@Table(name= "login")
+@SequenceGenerator(name = "seqLogin", sequenceName = "seq_login", initialValue = 50, allocationSize = 1)
+public class Login {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqLogin")
+	@Column(name="id_login")
+	private Long id;
+	@Column(name="login", length=100)
 	private String login;
-	private String motDePasse;
+	@Column(name="password", length=100)
+	private String motDePasse;	
+	@Column(name="admin")
 	private Boolean admin;
+	@OneToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name="id_client")
 	private Client client;
+	private int version;
 
 	public Login(String login, String motDePasse, Boolean admin) {
 		super();
@@ -27,8 +49,22 @@ public class Login {
 		super();
 	}
 
-	public void setId(Long id_login) {
-		this.id_login = id_login;
+	public Login(Long id, String login, String motDePasse, Boolean admin, Client client, int version) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.motDePasse = motDePasse;
+		this.admin = admin;
+		this.client = client;
+		this.version = version;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getLogin() {
@@ -55,10 +91,6 @@ public class Login {
 		this.admin = admin;
 	}
 
-	public Long getId() {
-		return id_login;
-	}
-
 	public Client getClient() {
 		return client;
 	}
@@ -66,5 +98,43 @@ public class Login {
 	public void setClient(Client client) {
 		this.client = client;
 	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Login other = (Login) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+	
 
 }
