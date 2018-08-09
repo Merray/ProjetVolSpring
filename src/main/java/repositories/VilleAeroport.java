9198c6b7301065b875faf03b0032274745fb8d45
Thibault <thibault.dufeu@gmail.com>
2018-08-09 11:48:58 +0200

    commit des repo Escale, VilleAeroport, CompagnieAerienneVol, Modif vol

---
+++ src/main/java/repositories/VilleAeroport.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.VilleAeroportPk;
+
+public interface VilleAeroport extends JpaRepository <VilleAeroport,VilleAeroportPk>{
+
+}
